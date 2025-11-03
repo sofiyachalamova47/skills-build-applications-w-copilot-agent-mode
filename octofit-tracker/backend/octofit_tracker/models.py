378b4c0d8e21ec5f70dae35a51ca01ab9afd7f1c
sofiyachalamova47 <sofiya.chalamova@accenture.com>
2025-11-03 15:24:15 +0000

    Update Django app for MongoDB, CORS, REST API, and collections support

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,33 @@
+from djongo import models
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'users'
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    members = models.JSONField()
+    class Meta:
+        db_table = 'teams'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    activity = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        db_table = 'activities'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=50)
+    points = models.IntegerField()
+    class Meta:
+        db_table = 'leaderboard'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'workouts'
